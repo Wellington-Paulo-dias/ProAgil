d5a9d9aa363bc5a1cd1090ccf5c3f876ac7bc2e2
Welllington-paulo-dias <divulguebetim@gmail.com>
2021-08-29 10:56:07 -0300

    Adicionado projeto angula SPA

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ProAgil.API.Data;
+using Microsoft.AspNetCore.Cors;
 
 namespace ProAgil.API
 {
@@ -30,6 +31,7 @@ namespace ProAgil.API
             services.AddDbContext<DataContext>(
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +48,7 @@ namespace ProAgil.API
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
